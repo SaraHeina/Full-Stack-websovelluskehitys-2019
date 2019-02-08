718a5ea8d003132ef0f94725f83d57ddc803350c
Sara Kauvo <sara.kau@hotmail.com>
2019-02-08 05:31:00 +0200

    osa4a

---
+++ Osa4/blogilista/app.js
@@ -0,0 +1,19 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const middleware = require('./utils/middleware')
+const mongoose = require('mongoose')
+const blogRouter = require('./controllers/blogs')
+
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.use('/api/blogs', blogRouter)
+app.use(middleware.unknownEndpoint)
+
+module.exports = app
